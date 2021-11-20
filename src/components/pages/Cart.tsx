0537b76cb6cf5649cccf4a5571d4ec6f48ae8808
Zachiah <zachiahsawyer1@gmail.com>
2021-11-20 15:32:27 -0800

    show no cart items text and make the cart items equality based on id

--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -1,11 +1,12 @@
 import { Component, For } from "solid-js";
 import { cart } from "../../data/cart";
+import { ProductRecord } from "../../ProductRecord";
 import ProductListing from "../ProductListing";
 
 const Cart : Component = () => {
 
     return (
-        <For each={[...new Set(cart())]} fallback={<p>You don't have any items in your cart ):</p>}>
+        <For each={[...new Set(cart().map(item => item.id))].map(item => cart().find(i => i.id === item)) as ProductRecord[]} fallback={<p class="p-4">You don't have any items in your cart ):</p>}>
             {(item) => 
                 <ProductListing product={item}/>
             }
